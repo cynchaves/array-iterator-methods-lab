ce0eb93eb7064e3c93f383aabee439a8573c59f0
Cynthia Chaves <cynthia_chaves@aol.com>
2024-09-02 12:56:05 -0400

    Completed Lab

--- app.js
+++ app.js
@@ -58,18 +58,18 @@ born in the 1500's.
   the years 1500 and 1599.
 */
 // Complete the exercise in the space below:
-// const veryOldInventors = inventors.filter((inventor) => {
-//       return inventor.year >= 1500 && inventor.year <= 1599; 
-// });
+const veryOldInventors = inventors.filter((inventor) => {
+      return inventor.year >= 1500 && inventor.year <= 1599; 
+});
 // Check your work:
-// console.log('Exercise 1 my result: ', veryOldInventors);
-// console.log(
-//   'Exercise 1 correct result: ',
-//   [
-//     { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-//     { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }
-//   ]
-// );
+console.log('Exercise 1 my result: ', veryOldInventors);
+console.log(
+  'Exercise 1 correct result: ',
+  [
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }
+  ]
+);
 
 /*
 Exercise 2: Array.prototype.map()
@@ -83,28 +83,28 @@ Hint: Return a new object literal from the callback that looks like:
       { first: "First Name", last: "Last Name" }
 */
 // Complete the exercise in the space below:
-// const inventorNames = inventors.map((inventor) => {
-//     return ({first: inventor.first, last: inventor.last});
-// });
+const inventorNames = inventors.map((inventor) => {
+    return ({first: inventor.first, last: inventor.last});
+});
 // Check your work:
-// console.log('Exercise 2 my result: ', inventorNames)
-// console.log(
-//   'Exercise 2 correct result: ',
-//   [
-//     { first: 'Albert', last: 'Einstein' },
-//     { first: 'Isaac', last: 'Newton' },
-//     { first: 'Galileo', last: 'Galilei' },
-//     { first: 'Marie', last: 'Curie' },
-//     { first: 'Johannes', last: 'Kepler' },
-//     { first: 'Nicolaus', last: 'Copernicus' },
-//     { first: 'Max', last: 'Planck' },
-//     { first: 'Katherine', last: 'Blodgett' },
-//     { first: 'Ada', last: 'Lovelace' },
-//     { first: 'Sarah E.', last: 'Goode' },
-//     { first: 'Lise', last: 'Meitner' },
-//     { first: 'Hanna', last: 'Hammarström' }
-//   ]
-// )
+console.log('Exercise 2 my result: ', inventorNames)
+console.log(
+  'Exercise 2 correct result: ',
+  [
+    { first: 'Albert', last: 'Einstein' },
+    { first: 'Isaac', last: 'Newton' },
+    { first: 'Galileo', last: 'Galilei' },
+    { first: 'Marie', last: 'Curie' },
+    { first: 'Johannes', last: 'Kepler' },
+    { first: 'Nicolaus', last: 'Copernicus' },
+    { first: 'Max', last: 'Planck' },
+    { first: 'Katherine', last: 'Blodgett' },
+    { first: 'Ada', last: 'Lovelace' },
+    { first: 'Sarah E.', last: 'Goode' },
+    { first: 'Lise', last: 'Meitner' },
+    { first: 'Hanna', last: 'Hammarström' }
+  ]
+)
 
 /*
 Exercise 3: Array.prototype.sort()
@@ -112,28 +112,28 @@ Sort the inventors by birth date in ascending order (from those born furthest in
 the past to those born most recently).
 */
 // Complete the exercise in the space below:
-// const sortedByBirthYear = inventors.sort((year1, year2) => {
-//     return year1.year - year2.year
-// });
+const sortedByBirthYear = inventors.sort((year1, year2) => {
+    return year1.year - year2.year
+});
 // Check your work:
-// console.log('Exercise 3 my result: ', sortedByBirthYear)
-// console.log(
-//   'Exercise 3 correct result: ',
-//   [
-//     { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-//     { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-//     { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-//     { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-//     { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
-//     { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
-//     { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-//     { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-//     { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-//     { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
-//     { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-//     { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 }
-//   ]
-// )
+console.log('Exercise 3 my result: ', sortedByBirthYear)
+console.log(
+  'Exercise 3 correct result: ',
+  [
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 }
+  ]
+)
 
 /* 
 Exercise 4: Array.prototype.find()
@@ -146,15 +146,15 @@ from an array of inventor objects
 - Assign the found inventor object to the variable inventorNamedAda
 */
 // Complete the exercise in the space below:
-// const inventorNamedAda = inventors.find((inventor) => {
-//   return inventor.first === 'Ada';
-// })
+const inventorNamedAda = inventors.find((inventor) => {
+  return inventor.first === 'Ada';
+})
 // Check your work:
-// console.log('Exercise 4 my result: ', inventorNamedAda)
-// console.log(
-//   'Exercise 4 correct result: ', 
-//   { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 }
-// )
+console.log('Exercise 4 my result: ', inventorNamedAda)
+console.log(
+  'Exercise 4 correct result: ', 
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 }
+)
 
 /*
 Exercise 5: Array.prototype.map()
@@ -166,13 +166,12 @@ Hint: Use the String.prototype.split() method to separate the first and last
       After splitting the names, rearrange them to the "First Last" format.
 */
 
+// let firstLast = []
+
 // Complete the exercise in the space below:
-//let firstLast = []
 
 
-testArray = 'Hello. How are you today?';
-testArray1 = testArray.split(' ');
-console.log(testArray1);
+
 // Check your work:
 // console.log('Exercise 5 my result: ', firstLast)
 // console.log(
@@ -221,6 +220,7 @@ console.log(testArray1);
 //   ]
 // )
 
+
 /*
 Exercise 6: Array.prototype.some()
 Determine if there is at least one person in the devs array who is 18 years 
@@ -231,12 +231,12 @@ old or older.
 - Store the result (true or false) in the variable 'isAdultPresent'. 
 */
 // Complete the exercise in the space below:
-// const isAdultPresent = devs.some((dev) => {
-//   return dev.year <= (2024 - 18);
-// });
+const isAdultPresent = devs.some((dev) => {
+  return dev.year <= (2024 - 18);
+});
 // Check your work:
-// console.log('Exercise 6 my result: ', isAdultPresent)
-// console.log('Exercise 6 correct result: ', true)
+console.log('Exercise 6 my result: ', isAdultPresent)
+console.log('Exercise 6 correct result: ', true)
 
 /*
 Exercise 7: Array.prototype.every()
@@ -249,14 +249,14 @@ Use Array.prototype.every() to determine if every person in the devs array is
   array is at least 19 years old.
 - Store the result (true or false) in the variable 'isEveryone19OrOlder'.
 */
-// let isEveryone19OrOlder = null
+let isEveryone19OrOlder = null
 // Complete the exercise in the space below:
-// isEveryone19OrOlder = devs.every((dev) => {
-//   return dev.year <= (2024 - 19);
-// });
+isEveryone19OrOlder = devs.every((dev) => {
+  return dev.year <= (2024 - 19);
+});
 // Check your work:
-// console.log('Exercise 7 my result: ', isEveryone19OrOlder)
-// console.log('Exercise 7 correct result: ', false)
+console.log('Exercise 7 my result: ', isEveryone19OrOlder)
+console.log('Exercise 7 correct result: ', false)
 
 /*
 Exercise 8: Array.prototype.find()
@@ -264,14 +264,14 @@ Use Array.prototype.find() to identify and retrieve the comment object with
 a specific ID 823423 from an array of comment objects.
 - Assign the found comment object to the variable 'commentById'.
 */
-// let commentById = {}
+let commentById = {}
 // Complete the exercise in the space below:
-// commentById = comments.find((comment) => {
-//   return comment.id === 823423;
-// });
+commentById = comments.find((comment) => {
+  return comment.id === 823423;
+});
 // Check your work:
-// console.log('Exercise 8 my result: ', commentById)
-// console.log('Exercise 8 correct result: ', { text: 'Super good', id: 823423 })
+console.log('Exercise 8 my result: ', commentById)
+console.log('Exercise 8 correct result: ', { text: 'Super good', id: 823423 })
 
 /*
 Exercise 9: Array.prototype.findIndex()
@@ -279,14 +279,14 @@ Determine the index of the comment that has a specific ID 123523 in an array
 of comment objects.
 - Store the index in the variable 'idx'.
 */
-// let idx = null
+let idx = null
 // Complete the exercise in the space below:
-// idx = comments.findIndex((comment) => {
-//   return comment.id === 123523;
-// })
+idx = comments.findIndex((comment) => {
+  return comment.id === 123523;
+})
 // Check your work:
-// console.log('Exercise 9 my result: ', idx)
-// console.log('Exercise 9 correct result: ', 3)
+console.log('Exercise 9 my result: ', idx)
+console.log('Exercise 9 correct result: ', 3)
 
 
 
