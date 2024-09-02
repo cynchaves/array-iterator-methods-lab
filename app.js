813737840e260073ba32dcb7bfedf430f9adeb9a
Cynthia Chaves <cynthia_chaves@aol.com>
2024-09-02 13:49:57 -0400

    Corrected Lab

--- app.js
+++ app.js
@@ -165,61 +165,58 @@ Hint: Use the String.prototype.split() method to separate the first and last
       names. You can split the string using ', ' as the separator.
       After splitting the names, rearrange them to the "First Last" format.
 */
-
-// let firstLast = []
-
 // Complete the exercise in the space below:
-
-
+let firstLast = people.map((person) => {
+  return person.split(', ').reverse().join(' ');
+});
 
 // Check your work:
-// console.log('Exercise 5 my result: ', firstLast)
-// console.log(
-//   'Exercise 5 correct result: ',
-//   [
-//     'Carl Becker',
-//     'Samuel Beckett',
-//     'Mick Beddoes',
-//     'Henry Beecher',
-//     'Ludwig Beethoven',
-//     'Menachem Begin',
-//     'Hilaire Belloc',
-//     'Saul Bellow',
-//     'Robert Benchley',
-//     'Peter Benenson',
-//     'David Ben-Gurion',
-//     'Walter Benjamin',
-//     'Tony Benn',
-//     'Chester Bennington',
-//     'Leana Benson',
-//     'Silas Bent',
-//     'Lloyd Bentsen',
-//     'Ric Berger',
-//     'Ingmar Bergman',
-//     'Luciano Berio',
-//     'Milton Berle',
-//     'Irving Berlin',
-//     'Eric Berne',
-//     'Sandra Bernhard',
-//     'Yogi Berra',
-//     'Halle Berry',
-//     'Wendell Berry',
-//     'Erin Bethea',
-//     'Aneurin Bevan',
-//     'Ken Bevel',
-//     'Joseph Biden',
-//     'Ambrose Bierce',
-//     'Steve Biko',
-//     'Josh Billings',
-//     'Frank Biondo',
-//     'Augustine Birrell',
-//     'Elk Black',
-//     'Robert Blair',
-//     'Tony Blair',
-//     'William Blake'
-//   ]
-// )
-
+console.log('Exercise 5 my result: ', firstLast)
+console.log(
+  'Exercise 5 correct result: ',
+  [
+    'Carl Becker',
+    'Samuel Beckett',
+    'Mick Beddoes',
+    'Henry Beecher',
+    'Ludwig Beethoven',
+    'Menachem Begin',
+    'Hilaire Belloc',
+    'Saul Bellow',
+    'Robert Benchley',
+    'Peter Benenson',
+    'David Ben-Gurion',
+    'Walter Benjamin',
+    'Tony Benn',
+    'Chester Bennington',
+    'Leana Benson',
+    'Silas Bent',
+    'Lloyd Bentsen',
+    'Ric Berger',
+    'Ingmar Bergman',
+    'Luciano Berio',
+    'Milton Berle',
+    'Irving Berlin',
+    'Eric Berne',
+    'Sandra Bernhard',
+    'Yogi Berra',
+    'Halle Berry',
+    'Wendell Berry',
+    'Erin Bethea',
+    'Aneurin Bevan',
+    'Ken Bevel',
+    'Joseph Biden',
+    'Ambrose Bierce',
+    'Steve Biko',
+    'Josh Billings',
+    'Frank Biondo',
+    'Augustine Birrell',
+    'Elk Black',
+    'Robert Blair',
+    'Tony Blair',
+    'William Blake'
+  ]
+)
 
 /*
 Exercise 6: Array.prototype.some()
